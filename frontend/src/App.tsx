d4bf9ac3848f2db801eba0d875ab49cd5e2ba58f
joshualan <joshualan23@gmail.com>
2023-08-13 16:24:51 -0400

    add absolute imports and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "lib/router";
+import { routes } from "routes";
 
 function App() {
+  const router = createBrowserRouter(routes);
+
   return (
     <div className="App">
-      <p>Alan Estrada's Fullstack Template</p>
+      <RouterProvider router={router} />
     </div>
   );
 }
