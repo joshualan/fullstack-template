c3708fc3a31cb502708b9f6bbdc6e10aed8104c4
joshualan <joshualan23@gmail.com>
2023-08-13 15:46:05 -0400

    add initial frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Alan Estrada's Fullstack Template</p>
+    </div>
+  );
+}
+
+export default App;
